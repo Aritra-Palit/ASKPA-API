0688309820bfed46423a2ece29b5160cd2e5a658
Aritra Palit <aritrapalit14@gmail.com>
2025-12-02 12:17:55 +0530

    Model for company info created.

---
+++ Model/clsCompanyInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace ASKPA_API.Model
+{
+    public class clsCompanyInfo
+    {
+        public string Code { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string AliasName { get; set; } = "";
+        public string Address1 { get; set; } = "";
+        public string Address2 { get; set; } = "";
+        public string State { get; set; } = "";
+        public string Country { get; set; } = "";
+        public string Pincode { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Mobile { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Contactno { get; set; } = "";
+        public string ContactPerson { get; set; } = "";
+        public string Website { get; set; } = "";
+        public string PANNO { get; set; } = "";
+        public string CINNO { get; set; } = "";
+        public string GSTNO { get; set; } = "";
+        public string EntryUser { get; set; } = "";
+        public int Active { get; set; } = 0;
+        public int Demo { get; set; } = 0;
+    }
+}
