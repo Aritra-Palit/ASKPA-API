99adc13a30d950d17a1816a2d1b4e152d5673563
Aritra Palit <aritrapalit14@gmail.com>
2025-12-02 12:23:57 +0530

    BLL and Controller for company.

---
+++ BLL/clsCompany.cs
@@ -0,0 +1,23 @@
+﻿using ASKPA.API.Common;
+using ASKPA_API.Model;
+using DocumentFormat.OpenXml.EMMA;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ASKPA_API.BLL
+{
+    public class clsCompany
+    {
+        public static async Task<string> NewCompany(string CompanyConnection, clsCompanyInfo info)
+        {
+            return await clsHelperDBAsync.fnDBOperationAsync(CompanyConnection, "PRC_CompanyOnboarding",
+               info.Code, info.Name, info.AliasName, info.Address1, info.Address2, info.State, info.Country, 
+               info.Pincode, info.Phone, info.Mobile, info.Email, info.Contactno, info.ContactPerson, info.Website, 
+               info.PANNO, info.CINNO,  info.GSTNO, info.EntryUser, info.Active, info.Demo);
+        }
+    }
+}
---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using ASKPA_API.BLL;
+using ASKPA_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+namespace ASKPA_API.Controllers
+{
+    [Route("askpa/admin")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        [HttpPost]
+        [Route("company/new")]
+        public async Task<IActionResult> NewCompanyDetails(clsCompanyInfo info)
+        {
+            string connection = HttpContext.Items["CompanyConnection"] as string;
+            var no = await clsCompany.NewCompany(connection, info);
+            return Ok(new { MerchantNumber = no });
+        }
+    }
+}
